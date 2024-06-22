5b5be7c594e906f6ed98799327b4a85c9164ba22
yotor1221 <133135091+yotor1221@users.noreply.github.com>
2024-06-22 17:46:29 +0300

    Create Replace elements with greatest element on ride side.py

---
+++ Replace elements with greatest element on ride side.py
@@ -0,0 +1,16 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        n = len(arr)
+        if n == 0:
+            return []
+        if n == 1:
+            return [-1]
+        
+        max_right = -1
+        
+        for i in range(n - 1, -1, -1):
+            current = arr[i]
+            arr[i] = max_right
+            max_right = max(max_right, current)
+        
+        return arr
